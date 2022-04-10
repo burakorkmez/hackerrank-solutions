bde565f685a05830e143ff07f99c0923344a8209
Burak Örkmez <burakorkmezz@gmail.com>
2022-04-10 22:33:47 +0300

    Electronics shop problem solution

---
+++ easy/electronics-shop.js
@@ -0,0 +1,14 @@
+function getMoneySpent(keyboards, drives, b) {
+	let max = -1;
+
+	for (let i = 0; i < keyboards.length; i++) {
+		const keyboardPrice = keyboards[i];
+		for (let j = 0; j < drives.length; j++) {
+			const drivePrice = drives[j];
+			if (keyboardPrice + drivePrice <= b && keyboardPrice + drivePrice > max) {
+				max = keyboardPrice + drivePrice;
+			}
+		}
+	}
+	return max;
+}
